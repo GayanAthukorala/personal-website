ea4ca820dc5c90d5612b07c8fd490da5ebf30798
GayanAthukorala <gayan42004@gmail.com>
2023-04-17 03:23:34 -0400

    Updated Menu with Animations

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import logo from "./logo.svg";
+import gayanPhoto from "./images/Gayan.png";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <div id="menu">
-        <a href="#about" className="menu-item">
-          ABOUT
-        </a>
-        <a href="#projects" className="menu-item">
-          PROJECTS
-        </a>
-        <a href="#contact" className="menu-item">
-          CONTACT
-        </a>
+      <div className="menu">
+        <div className="contents-bar">
+          <div id="bar" />
+        </div>
+        <div id="menu-items">
+          <div className="row">
+            <a href="#about" className="menu-item">
+              ABOUT
+            </a>
+            <div className="bar-peg" />
+          </div>
+
+          <div className="row">
+            <a href="#projects" className="menu-item">
+              PROJECTS
+            </a>
+            <div className="bar-peg" />
+          </div>
+          <div className="row">
+            <a href="#contact" className="menu-item">
+              CONTACT
+            </a>
+            <div className="bar-peg" />
+          </div>
+        </div>
+        <div className="pictures-wrapper">
+          <div className="profile-background">
+            <div className="image-cropper">
+              <img src={gayanPhoto} style={{ height: 350, width: 350 }} />
+            </div>
+          </div>
+        </div>
       </div>
       <div id="about">
         <div>WHO AM I?</div>
